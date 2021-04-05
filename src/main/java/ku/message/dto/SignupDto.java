8778843563a9b4edfdf048c09089bfcc20b463a0
Thanida Jongarnon <bameejrn@outlook.com>
2021-04-05 13:15:38 +0700

    Add email input validation

--- src/main/java/ku/message/dto/SignupDto.java
+++ src/main/java/ku/message/dto/SignupDto.java
@@ -1,9 +1,12 @@
+
+
 package ku.message.dto;
 
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -18,10 +21,16 @@ public class SignupDto {
     private String password;
 
     @NotBlank(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$",
+            message = "First name can only contain letters")
     private String firstName;
 
     @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
 
     private String role;
 }
